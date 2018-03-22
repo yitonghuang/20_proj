94fdb44de5968d565349190e1168f69ceb54880a
smille23 <31680796+smille23@users.noreply.github.com>
2018-03-22 16:54:38 -0400

    Update setup_2.R

--- setup_2.R
+++ setup_2.R
@@ -1,6 +1,7 @@
-#setup for median household Income
 census <- read.csv("nyc_census.csv")
-to.remove <- c("IncomeErr", "IncomePerCap", "IncomePerCapErr", "Poverty", "ChildPoverty", "Native", "Office", "Carpool", "PublicWork")
+census = data.frame(census, with(census, Men/TotalPop*100))
+names(census)[37] <- "PercentMale"
+to.remove <- c("IncomeErr", "IncomePerCap", "IncomePerCapErr", "Men", "Women", "Poverty", "ChildPoverty", "Native", "Office", "Carpool", "PublicWork")
 `%ni%` <- Negate(`%in%`)
 census <- subset(census,select = names(census) %ni% to.remove)
 census <- na.omit(census)
@@ -9,13 +10,14 @@ census <- na.omit(census)
 
 #setup for income per capita
 census2 <- read.csv("nyc_census.csv")
-to.remove <- c("IncomeErr", "Income", "IncomePerCapErr", "Poverty", "ChildPoverty", "Native", "Office", "Carpool", "PublicWork")
+census2 = data.frame(census2, with(census2, Men/TotalPop*100))
+names(census2)[37] <- "PercentMale"
+to.remove <- c("IncomeErr", "Income", "IncomePerCapErr", "Men", "Women", "Poverty", "ChildPoverty", "Native", "Office", "Carpool", "PublicWork")
 `%ni%` <- Negate(`%in%`)
 census2 <- subset(census2,select = names(census2) %ni% to.remove)
 census2 <- na.omit(census2)
 #uncomment if you wish to save
 #write.csv(census2, file = "incomePerCap_setup.csv")
-
 
 
 
